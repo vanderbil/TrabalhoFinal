d599fcea9047a646c36156b0fa71f08954a67739
Vanderbil <vanderbil@bol.com.br>
2022-01-08 00:22:37 -0300

    Abstractfactory
    Abstractfactory

--- Pattern/Trabalho/src/Main.java
+++ Pattern/Trabalho/src/Main.java
@@ -1,5 +1,5 @@
 
-
+import AbstarctFactory.Main;
 
 import java.util.Scanner;
 
@@ -7,10 +7,30 @@ import java.util.Scanner;
 public class Main {
       public static void main(String [] args) {
       Scanner ler = new Scanner(System.in);
-	int num;
+	
 	
 	System.out.printf("Informe o primeiro valor: ");
 	
+	int num;
+	
+	if(num == 1) {
+AbstractFactory offRoadFactory = FactoryProducer.getFactory("OffRoad");
+		
+		Veiculo carro1 = offRoadFactory.getVeiculo("X");
+		carro1.method();
+		
+		Veiculo carro2 = offRoadFactory.getVeiculo("Y");
+		carro2.method();
+		
+		AbstractFactory streetFactory = FactoryProducer.getFactory("Street");
+		
+		Veiculo carro3 = streetFactory.getVeiculo("X");
+		carro3.method();
+		
+		Veiculo carro4 = streetFactory.getVeiculo("Y");
+		carro4.method();
+		
+	}
 	
 }
 
